04e363d010566ced2e7de68c6c2a831b0fe53d37
Sarah Hart <sarah.hart.nc@gmail.com>
2022-04-13 19:48:31 -0400

    updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS position;
+DROP TABLE IF EXISTS positions;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
@@ -12,7 +12,7 @@ CREATE TABLE positions (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER,
+  department_id INTEGER
 );
 
 CREATE TABLE employee (
@@ -20,7 +20,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  manager_id INTEGER NOT NULL,
+  manager_id INTEGER NOT NULL
 );
 
 -- CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
